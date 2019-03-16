1646acf04c3f7d48247edc1cfa664b88bf47cca5
daohuytuan <tuandh95@gmail.com>
2019-03-17 00:00:34 +0700

    fix heroku

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from "./component/headers";
 import { BrowserRouter, Route,Switch} from "react-router-dom";
-import Cart from "./component/Cart";
+import Cart from "./component/Cart.jsx";
 import ListItem from './component/ListItem';
 import ProductDetail from "./component/Page/ProductDetail";
 export default class App extends Component {
